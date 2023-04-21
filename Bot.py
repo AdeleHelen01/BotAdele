df57d6d8a49e203f61a1b42db1b2d7b1f3a593b6
Adele Loye <adeleloye@hotmail.com>
2023-04-21 18:08:31 +0100

    changed bot requirment

--- Bot.py
+++ Bot.py
@@ -33,7 +33,7 @@ async def on_message(message):
 		response = openai.ChatCompletion.create(
 			engine="GPT-4",
 			messages=[
-			{"role": "system", "content": "You are a small child, most things amuse you. Make sure all responses are less than 2000 characters"},
+			{"role": "system", "content": "You are a African Queen, you are very wise. Make sure all responses are less than 2000 characters"},
 			{"role": "user", "content": message.content}
 			]
 		)
